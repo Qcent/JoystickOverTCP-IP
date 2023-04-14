6b6f43c1ee5480be54e3659d45c6cc3a340a5fc1
Qcent <dquinn8@cogeco.ca>
2023-04-14 08:01:37 -0400

    added arguments, error handling
    refactored slightly, added latency output , command line arguments and error handling

--- joyServer.py
+++ joyServer.py
@@ -2,10 +2,30 @@ import socket
 import ast
 import pyvjoy
 import urllib.request
+import time
+import argparse
 
+# Create an argument parser
+parser = argparse.ArgumentParser(description='Receive joystick data from a computer over tcp/ip')
+
+# Add arguments to the parser
+parser.add_argument('-p', '--port', type=str, help='Port that you would like to listen on')
+parser.add_argument('-l', '--latency', action='store_true', help='Show latency output')
+
+# Parse the arguments
+args = parser.parse_args()
+
+HOST = '0.0.0.0'    # listen on all addresses
+# set port
+if args.port:
+    PORT = int(args.port)
+else:
+    PORT = 5000
+
+# identify internal and external ip addresses
+hostname = socket.gethostname()
+lan_ip = socket.gethostbyname(hostname)
 external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-HOST = '0.0.0.0'
-PORT = 5000
 
 # establish server socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,16 +36,33 @@ server_socket.listen(1)
 # initialize vJoy joystick
 joystick = pyvjoy.VJoyDevice(1)
 
+
 def awaitConnection():
-    print(f'Waiting for Connection on {external_ip}')
+    print(f'Waiting for Connection on port: {PORT}\n\t\t LAN: {lan_ip}\n\t\t WAN: {external_ip}')
 
     # accept client connection
     client_socket, client_address = server_socket.accept()
     print(f'Connection from {client_address} established')
 
+    response = 'Ok'
+    client_timing = int(client_socket.recv(1024).decode())
+    client_socket.sendall(response.encode())
+    loop_count = 0
+    print(f'Client Timing: {client_timing}fps established')
+
     # receive joystick input from client
     while True:
-        data = client_socket.recv(1024)
+        try:
+            data = client_socket.recv(1024)
+            if args.latency:
+                loop_count += 1
+                if loop_count == 20:    # 20 round trips completed
+                    end_time = time.time()
+                    print(f'Latency: {((end_time - start_time )*1000) - 1000/client_timing : .2f} ms')
+                    loop_count = 0
+        except Exception:
+            break
+
         if not data:
             break
 
@@ -59,8 +96,12 @@ def awaitConnection():
             joystick.set_disc_pov(1, -1)
 
         # send response back to client
-        response = 'Ok'
-        client_socket.sendall(response.encode())
+        try:
+            client_socket.sendall(response.encode())
+            if args.latency and loop_count == 19:
+                start_time = time.time()        # let's calculate some latency
+        except Exception:
+            break
 
     # close connection
     client_socket.close()
@@ -68,6 +109,7 @@ def awaitConnection():
 
     # reset vJoy joystick
     joystick.reset()
+
 
 while True:
     awaitConnection()
